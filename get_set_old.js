c81ee20cd01718d26bb3aeeb1f1ced4e6aff3e80
Manish5043 <153104132+Manish5043@users.noreply.github.com>
2024-09-04 23:30:30 +0530

    Add files via upload

---
+++ get_set_old.js
@@ -0,0 +1,46 @@
+function user(email, password){
+    this._email = email;
+    this._password = password
+
+    Object.defineProperty(this, 'email',{
+        get: function(){
+            return this._email.toUpperCase()
+        },
+        set: function(value){
+            this._email = value
+        }
+    })
+    Object.defineProperty(this, 'password',{
+        get: function(){
+            return this._password.toUpperCase()
+        },
+        set: function(value){
+            this._password = value
+        }
+    })
+}
+
+const chai = new user("@chai.com", "chai")
+
+console.log(chai.email);
+
+
+
+
+// object type of declaration
+
+const user = {
+    _email: "h@hc.com",
+    _password: "abc",
+
+    get email(){
+        return this._email.toUpperCase()
+    },
+
+    set email(value){
+        this._email= value
+    }
+}
+
+const tea = Object.create(user)
+console.log(tea.email());
